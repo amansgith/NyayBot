886599a0ce955e5e87b6ed07b7d41207d4741092
Aman Khan <amankhancompq2002@gmail.com>
2025-02-16 02:17:19 +0530

    hero section layout change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,20 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero-section flex'>
-        <DotLottieReact className='herogif'
-      src="https://lottie.host/8dc3dbaf-96f5-4586-819e-3e1ed4290cb6/NhsmMchDOG.lottie"
-      loop
-      autoplay
-    />
-    <h1 className='text-8xl w-full text-purple-300'>Your all Legal Solutions at one Place</h1>
+    <div className='hero-section flex relative'>
+        <img className='bgimg' src="https://cdn.pixabay.com/photo/2024/04/06/17/32/ai-generated-8679700_1280.jpg" alt="" />
+        <div className="textbg">
+        <h1 className='blur-overlay absolute top-0 right-0 bottom-0 herotext w-1/2 text-white'>Find all your Legal Solutions at one Place</h1>
+        </div>
     </div>
   )
 }
 
 export default Hero
+
+
+{/* <DotLottieReact className='herogif w-1/2 pt-20'
+src="https://lottie.host/8dc3dbaf-96f5-4586-819e-3e1ed4290cb6/NhsmMchDOG.lottie"
+loop
+autoplay
+/> */}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import law from '../../assets/law.png';
+import logo from '../../assets/law.png';
 import profile from '../../assets/profile.png'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <>
-        <nav className='flex justify-between text-2xl p-4 shadow-lg'>
+        <nav className='flex justify-between text-2xl p-6 pl-8 pr-8'>
             <div className="brand flex gap-2">
-                <img src={law} alt="Logo" />
+                <img src={logo} alt="Logo" />
                 <h1 className="brand-name text-3xl">NyayBot</h1>
             </div>
-            <div className="nav-items flex gap-5 text-xl">
+            <div className="nav-items flex gap-5 text-1xl justify-center">
                 <Link to="/" className="nav-item">Home</Link>
                 <Link to="/conversation" className="nav-item">Chatbot</Link>
-                <Link to="/signup" className="nav-item">Signup</Link>
+                <Link to="/signup" className="nav-item">Appointment</Link>
                 <img src={profile} alt="profile" />
             </div>
         </nav>
